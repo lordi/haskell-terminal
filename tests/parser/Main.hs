56452dc37d0f723a2c624e1600aacca3f7ad6f10
Hannes Gräuler <graeuler@geoplex.de>
2013-06-05 18:30:37 +0200

    Add some structure to the parser tests

--- tests/parser/Main.hs
+++ tests/parser/Main.hs
@@ -1,6 +1,3 @@
--- |This file contains tests to validate the parser, e.g., that the
--- incoming character stream is correctly translated into
--- TerminalActions.
 module Main where
 import Data.Monoid
 import Control.Applicative ((<$>))
@@ -15,7 +12,35 @@ import Terminal.Parser
 import Terminal.Terminal
 import Terminal.Types
 
--- |An InputStream will be parsed into TerminalActions
+-- |This section contains unit tests to validate the parser, e.g., to ensure
+-- that the incoming character stream is correctly translated into
+-- `TerminalAction`s. This fact is clearly expressed by the function signature
+-- of the assertion function `parsesTo` which is used in the assertions below.
+parsesTo :: String -> [TerminalAction] -> Assertion
+parsesTo str is = let Right result = parseANSI str in result @?= (is, "")
+
+testSetCursor = "A\ESC[H\ESC[2;2H"
+        `parsesTo` [CharInput 'A', SetCursor 1 1, SetCursor 2 2]
+
+testInvalidSetCursor = "\ESC[H\ESC[2;2;X\ESC[5;1H"
+        `parsesTo` [SetCursor 1 1, Ignored, SetCursor 5 1]
+
+testMoveCursor = "A\ESC[A\ESCA\ESC[10B"
+        `parsesTo` [CharInput 'A', CursorUp 1, Ignored, CursorDown 10]
+
+testCharInput = "A2$?"
+        `parsesTo` [CharInput 'A', CharInput '2', CharInput '$', CharInput '?']
+
+unitTests =
+       [ testCase "testSetCursor" testSetCursor
+       , testCase "testInvalidSetCursor" testInvalidSetCursor
+       , testCase "testMoveCursor" testMoveCursor
+       , testCase "testCharInput" testCharInput
+       ]
+
+-- |The second section of this file consists of QuickCheck properties to ensure
+-- that the peraser is rebust again arbirtrary input. Therefore a InputStream
+-- type is defined which represents the incoming character stream.
 newtype InputStream = InputStream String deriving Show
 
 instance Arbitrary InputStream where
@@ -31,25 +56,11 @@ prop_NotManyCharsLeftOver (InputStream str) =
     let Right (x, s) = parseANSI str in
     length s < 15
 
-testSetCursor :: Assertion
-testSetCursor = let Right result = parseANSI "A\ESC[H\ESC[2;2H" in
-          result @?= ([CharInput 'A', SetCursor 1 1, SetCursor 2 2], "")
+propertyTests =
+       [ testProperty "parseNotManyCharsLeftOver" prop_NotManyCharsLeftOver
+       ]
 
-testInvalidSetCursor :: Assertion
-testInvalidSetCursor = let Right result = parseANSI "\ESC[H\ESC[2;2;X\ESC[5;1H" in
-          result @?= ([SetCursor 1 1, Ignored, SetCursor 5 1], "")
-
-testMoveCursor :: Assertion
-testMoveCursor = let Right result = parseANSI "A\ESC[A\ESCA\ESC[10B" in
-          result @?= ([CharInput 'A', CursorUp 1, Ignored, CursorDown 10], "")
-
+-- Finally, execute the tests with the cabal test framework
 main :: IO ()
-main = defaultMainWithOpts
-       [ testCase "testSetCursor" testSetCursor
-       , testCase "testInvalidSetCursor" testInvalidSetCursor
-       , testProperty "parseNotManyCharsLeftOver" prop_NotManyCharsLeftOver
-       ] mempty
-
-
-
+main = defaultMainWithOpts (unitTests ++ propertyTests) mempty
 
